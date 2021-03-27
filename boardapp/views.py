f1cd786f356b0cff3fa0e528a27b7d6b3bc98c9c
elbarca1969 <72918547+elbarca1969@users.noreply.github.com>
2021-03-27 12:48:24 +0900

    ログアウト機能の実装

--- boardapp/views.py
+++ boardapp/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.db import IntegrityError
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from .models import BoardModel
 from django.contrib.auth.decorators import login_required
 
@@ -33,3 +33,7 @@ def loginfunc(request):
 def listfunc(request):
     object_list = BoardModel.objects.all()
     return render(request, 'list.html', {'object_list':object_list})
+
+def logoutfunc(request):
+    logout(request)
+    return redirect('login')
